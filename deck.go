a6f357894dca24665e2b33576c4926f420a36334
Taiwen <kimdaxterkid@gmail.com>
2018-12-19 16:43:44 +0800

    Deck methods

--- deck.go
+++ deck.go
@@ -6,6 +6,19 @@ import "fmt"
 // which is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
 // all deck type varibale could use print() function
 // d is abbreviation for deck, usually use the first letter of the type, this case - 'd'
 func (d deck) print() {
